c70f1798d5db43a1cb447bb06ab0ba6865524460
AlexVG <AlexVG@WS-683>
2016-08-11 11:29:52 +0300

    FirstCommit

---
+++ AnaliticProject/AnaliticProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnaliticProject.Startup))]
+namespace AnaliticProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
